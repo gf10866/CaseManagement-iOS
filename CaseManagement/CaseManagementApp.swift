c8f861b460edf3e46125d1d03def6b6c9f6d9fa3
gaofeng <Gaofeng@olaparty.sg>
2026-02-09 16:15:22 +0800

    Initial Commit

---
+++ CaseManagement/CaseManagementApp.swift
@@ -0,0 +1,17 @@
+//
+//  CaseManagementApp.swift
+//  CaseManagement
+//
+//  Created by gaofeng on 2026/2/9.
+//
+
+import SwiftUI
+
+@main
+struct CaseManagementApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CaseManagement/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  CaseManagement
+//
+//  Created by gaofeng on 2026/2/9.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
